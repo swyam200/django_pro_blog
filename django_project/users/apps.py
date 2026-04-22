3835c8764db48dd20a621d4f1237bdf9235a23bf
Swyam Balaiwar <kg791026@gmail.com>
2026-04-22 11:36:15 +0530

    Fix apps.py correctly

--- django_project/users/apps.py
+++ django_project/users/apps.py
@@ -1,9 +1,8 @@
 from django.apps import AppConfig
 
-
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name ='django_project.blog'
+    name = 'django_project.users'   # ✅ FIXED
 
     def ready(self):
-       import users.signals
+        from . import signals   # ✅ BEST PRACTICE
