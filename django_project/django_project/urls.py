8df39c8bcea3bce7fc48229d43e0820265171d63
Swyam Balaiwar <kg791026@gmail.com>
2026-04-22 12:22:40 +0530

    Fix import paths in urls.py

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from users import views as user_views
+from django_project.users import views as user_views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -30,7 +30,7 @@ urlpatterns = [
     path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html' ),name='password_reset_confirm'),
     path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),name='password_reset_complete'),
     path('profile/',user_views.profile,name='profile'),
-    path('',include('blog.urls')),
+    path("", include("django_project.blog.urls")),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
